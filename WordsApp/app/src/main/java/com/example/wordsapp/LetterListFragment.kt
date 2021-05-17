aec331e93f49cbb421e698ac75014645966060ed
Shaun Nguyen <snguyendohoang@gmail.com>
2021-05-17 00:08:38 -0700

    Implement Fragments
    implemented LetterListFragment and WordListFragment
    
    migrated codes from MainActivity -> LetterListFragment and Detail_Activity -> WordListFragment
    
    deleted Detail_Activity.kt and activity_detail.xml

---
+++ WordsApp/app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -0,0 +1,98 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.FragmentLetterListBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LetterListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LetterListFragment : Fragment() {
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
+    // get() means the property is "get-only". You can get the value, but once assigned, you cannot assign it to something else
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    /* Inflate the layout */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    /* Do operations on the view just created */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
+        chooseLayout()
+    }
+
+    /* Destroy view */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /* Inflate Option menu layout */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+
+    /* Do operations with the Option Menu just created */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /* Set layout invoked by LayoutManager */
+    private fun chooseLayout(){
+        when(isLinearLayoutManager){
+            true -> {
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = LetterAdapter()
+            }
+            false -> {
+                recyclerView.layoutManager = GridLayoutManager(context, 4)
+                recyclerView.adapter = LetterAdapter()
+            }
+        }
+    }
+
+    /* Set icon based on LayoutManager */
+    private fun setIcon(menuItem: MenuItem){
+        if(menuItem==null)
+            return
+
+        menuItem.icon =
+            if(isLinearLayoutManager)
+                ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
+    }
+}
